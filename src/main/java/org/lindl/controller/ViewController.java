8cfa1880f498b5e3b9315868c50bc8f98c0e0b65
lindl <lin_donglian@163.com>
2017-04-12 17:00:52 +0800

    视图跳转控制

--- src/main/java/org/lindl/controller/ViewController.java
+++ src/main/java/org/lindl/controller/ViewController.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by lin on 2017/1/29.
@@ -13,19 +15,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ViewController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    /**
-     * 测试hello
-     * @return
-     */
-    @RequestMapping(value = "/view",method = RequestMethod.GET)
-    public String hello(Model model) {
-//        model.addAttribute("name", "Dear");
-        return "page1";
-    }
 
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index(){
         return "index";
+    }
+    @RequestMapping(value = "/view/{id}",method = RequestMethod.GET)
+    public String redirectToPage(@PathVariable("id") String id){
+        return id;
     }
 
 }
